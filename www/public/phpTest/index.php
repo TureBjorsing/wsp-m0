792e7cb3ffe3efa518a92b7fd0f0385828b3fd08
krimsnet1337 <06bjtu@skola.engelholm.se>
2024-09-02 14:14:35 +0200

    init

---
+++ www/public/phpTest/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP - TEST</h1>
+    <?php
+      $version = PHP_VERSION;
+      echo "<h2>Hello PHP " . $version . "<h2>";
+
+      $num1 = 3;
+      $num2 = 2;
+      $numT = $num1 + $num2;
+
+      echo "<h2>Num is " . $numT . "<h2>"; 
+    ?>
+</body>
+</html>
