127ea954f6446a2696e073ef27fa880a91368e04
krimsnet1337 <06bjtu@skola.engelholm.se>
2024-09-09 10:24:39 +0200

    Nya frågor

--- www/public/phpTest/evaluate.php
+++ www/public/phpTest/evaluate.php
@@ -5,16 +5,26 @@
 <h1>Matematiktest</h1>
 <h2>Resultat</h2>
 <?php
+		$namn = $_POST['namn'];
      	$ans1 = $_POST['q1'];
      	$ans2 = $_POST['q2'];
+		$ans3 = $_POST['q3'];
+		$ans4 = $_POST['q4'];
+		$ans5 = $_POST['q5'];
      	$points = 0;
      	
      	if($ans1 == 9)
            $points++;
      	if($ans2 == 15)
            $points++;
-     	
-     	echo("<p>Du fick " . $points . " av 2 möjliga</p>");
+		if($ans3 == "hunden")
+           $points++;
+		if($ans4 == 50)
+           $points++;
+		if($ans5 == 3)
+           $points++;
+     	echo("<p>Du fick " . $points . " av 5 möjliga</p>");
+		echo("<p>Bra jobbat " . $namn . "!");
 ?>
 </body>
 </html>
